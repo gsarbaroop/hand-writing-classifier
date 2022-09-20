346ea28d65fddcfba05438cc7c756a446d518a70
Sarbaroop Ghosh <ghosh.12@iitj.ac.in>
2022-09-21 00:10:42 +0530

    HyperParameter tuning

--- hand_writing_recog.py
+++ hand_writing_recog.py
@@ -3,6 +3,21 @@ import matplotlib.pyplot as plt
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from skimage import data, color
+from skimage.transform import rescale, resize, downscale_local_mean
+import pandas as pd
+from PIL import Image
+
+gamma_list=[0.001, 0.05, 0.003, 0.0002, 0.00001]
+c_list = [0.1, 0.8, 0.3, 2, 8]
+
+h_param_comb = [{'gamma':g, 'C':c} for g in gamma_list for c in c_list]
+
+train_frac = 0.8
+test_frac = 0.1
+dev_frac = 0.1
 
 digits = datasets.load_digits()
 
@@ -16,35 +31,66 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+digits = datasets.load_digits()
+data = digits.images.reshape((n_samples, -1))
+print("---Digits--")
+plt.figure(1, figsize=(3, 3))
+plt.imshow(digits.images[0], cmap=plt.cm.gray_r, interpolation="nearest")
+plt.show()
+print(digits.images[0].size)
 
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False
+dev_test_frac = 1-train_frac
+X_train, X_dev_test, y_train, y_dev_test = train_test_split(
+    data, digits.target, test_size=0.5, shuffle=True
 )
 
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+X_test, X_dev, y_test, y_dev = train_test_split(
+    X_dev_test, y_dev_test, test_size=(dev_frac)/dev_test_frac, shuffle=True
+)
 
-# Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+df = pd.DataFrame()
+gm = []
+cr =[]
+acc =[]
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+for gamma in gamma_list:
+    for c in c_list:
+        # Create a classifier: a support vector classifier
+        clf = svm.SVC(gamma=gamma, C=c)
+        
+        # Learn the digits on the train subset
+        clf.fit(X_train, y_train)
+
+        # Predict the value of the digit on the test subset
+        predicted = clf.predict(X_dev)
+        
+        score = accuracy_score(y_pred=predicted,y_true=y_dev)
+        
+        gm.append(gamma)
+        cr.append(c)
+        acc.append(score)
+        
+        
+df['Gamma'] = gm
+df['C']= cr
+df['Accuracy'] = acc
+
+print(df)
+
+_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 10))
 for ax, image, prediction in zip(axes, X_test, predicted):
     ax.set_axis_off()
-    image = image.reshape(8, 8)
+    image = image.reshape(8,8)
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
+    image_resized = resize(image, (int(image.shape[0] // 4), int(image.shape[1] // 2)),
+                       anti_aliasing=True)
+    
+    ax.set_title(f"Prediction  : {prediction}" f"ImageSize: {image.size}")
+    print("the image with the size" f"Prediction: {prediction}" f"image_resized :{image_resized}\n")
 
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
+accuracy= df['Accuracy']
+max = accuracy.max()
+index = accuracy.idxmax()
 
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
+print("The best test score is ", max," corresponding to hyperparameters gamma= ",gm[index]," C=",cr[index])
 plt.show()
-
